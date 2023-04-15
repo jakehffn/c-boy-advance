054a100f4343c4a9023ffab65d69ef9da028d97d
Jake Hoffman <jaketh19@gmail.com>
2023-04-15 11:35:59 -0400

    Added escape to exit

--- src/main.c
+++ src/main.c
@@ -18,32 +18,36 @@ void pollEvents() {
     // Poll for events
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
-            case SDL_QUIT:
-                // User clicked the close button, exit loop
-                printf("Quit event received\n");
+        case SDL_QUIT:
+            // User clicked the close button, exit loop
+            printf("Quit event received\n");
+            quit = true;
+            break;
+
+        case SDL_WINDOWEVENT:
+
+            if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                glViewport(0, 0, event.window.data1, event.window.data2);
+            }
+            break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym) {
+            case SDLK_ESCAPE:
                 quit = true;
                 break;
-
-            case SDL_WINDOWEVENT:
-
-                if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-                    glViewport(0, 0, event.window.data1, event.window.data2);
-                }
-                break;
-
-            case SDL_KEYDOWN:
-                // User pressed a key
-                printf("Key pressed: %s\n", SDL_GetKeyName(event.key.keysym.sym));
-                break;
-
-            case SDL_MOUSEBUTTONDOWN:
-                // User clicked the mouse button
-                printf("Mouse button %d clicked at (%d,%d)\n", event.button.button, event.button.x, event.button.y);
-                break;
-                
+            
             default:
-                // Ignore other events
                 break;
+            }
+            break;
+
+        case SDL_MOUSEBUTTONDOWN:
+            printf("Mouse button %d clicked at (%d,%d)\n", event.button.button, event.button.x, event.button.y);
+            break;
+            
+        default:
+            break;
         }
     }
 }
@@ -63,8 +67,6 @@ int main(UNUSED int argv, UNUSED char** args) {
     } else {
         
         #ifdef __EMSCRIPTEN__
-
-            printf("On the web!\n");
 
             emscripten_set_main_loop(mainLoop, 0, 1);
 
